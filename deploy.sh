44a82ba3164cb8d8cf234955894cd3192e167ab9
Chase Dorsey <git@chase-dorsey.com>
2016-12-16 09:42:52 -0500

    Reverted change

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@ pwd
 ls
 echo "${SSH_KEY}" > ssh_key
 chmod 400 ./ssh_key
-spawn "scp -i ./ssh_key ./organicbot $REMOTE_FILE"
-
-expect "Enter passphrase for key './ssh_key': " { send "\r" }
+scp -i ./ssh_key ./organicbot $REMOTE_FILE
