c3ebd10aff4e33cf9deb4324b51b415798350acf
Chase Dorsey <git@chase-dorsey.com>
2016-12-16 09:34:37 -0500

    Added automated response for passphrase

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,6 @@ pwd
 ls
 echo "${SSH_KEY}" > ssh_key
 chmod 400 ./ssh_key
-scp -i ./ssh_key ./organicbot $REMOTE_FILE
+spawn "scp -i ./ssh_key ./organicbot $REMOTE_FILE"
+
+expect "Enter passphrase for key './ssh_key': " { send "\r" }
