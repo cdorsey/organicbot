6df4665802270e313871c3d1f4833f6b57cbf898
Chase Dorsey <git@chase-dorsey.com>
2016-12-16 08:45:17 -0500

    Added debugging commands

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+pwd
+ls
 cat $SSH_KEY > ssh_key
-go build -o ./organicbot $GOPATH/src/github.com/cdorsey/organicbot
+# go build -o ./organicbot $GOPATH/src/github.com/cdorsey/organicbot
 scp -i ./ssh_key ./organicbot $REMOTE_FILE
